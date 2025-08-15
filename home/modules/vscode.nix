3643fc7c179d0c8ed6a4a16e901b7a9572e6b0bb
BCR <57232475+bcr-ch@users.noreply.github.com>
2025-08-15 07:25:42 +0200

    vscode added

---
+++ home/modules/vscode.nix
@@ -0,0 +1,10 @@
+{pkgs, ...}: {
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode.fhs;
+    extensions = with pkgs.vscode-extensions; [
+      dracula-theme.theme-dracula
+      vscodevim.vim
+    ];
+  };
+}
