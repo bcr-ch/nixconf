07412e871fd0bdd5f5d7160c768c6024803b9e95
BCR <57232475+bcr-ch@users.noreply.github.com>
2025-08-18 08:08:55 +0200

    clean mac home

--- home/macos-home.nix
+++ home/macos-home.nix
@@ -50,53 +50,53 @@
     };
   };
   
-  programs.chromium = {
-    enable = false;
-    package = pkgs.brave;
-    extensions = [
-      { id = "ddkjiahejlhfcafbddmgiahcphecmpfh"; } #ublock origin light
-      { id = "bkdgflcldnnnapblkhphbgpggdiikppg"; } #duckduckgo
-      { id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa"; } #1password
-    ];
-    commandLineArgs = [
-      "--disable-features=WebRtcAllowInputVolumeAdjustment"
-    ];
+#  programs.chromium = {
+#    enable = false;
+#    package = pkgs.brave;
+#    extensions = [
+#      { id = "ddkjiahejlhfcafbddmgiahcphecmpfh"; } #ublock origin light
+#      { id = "bkdgflcldnnnapblkhphbgpggdiikppg"; } #duckduckgo
+#      { id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa"; } #1password
+#    ];
+#    commandLineArgs = [
+#      "--disable-features=WebRtcAllowInputVolumeAdjustment"
+#    ];
     #homepageLocation = "duckduckgo.com";
     #defaultSearchProviderSearchURL = "https://duckduckgo.com/?q={searchTerms}";
-  };
+#  };
 
 
 
-  programs.firefox = {
-    enable = false;
-    policies = { 
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      EnableTrackingProtection = {
-        Value = true;
-        Locked = true;
-        Cryptomining = true;
-        Fingerprinting = true;
-      };
-      DisablePocket = true;
-      DisableFirefoxAccounts = true;
-      DisableAccounts = true;
-      DisableFirefoxScreenshots = true;
-      OverrideFirstRunPage = "";
-      OverridePostUpdatePage = "";
-      DontCheckDefaultBrowser = true;
+#  programs.firefox = {
+#    enable = false;
+#    policies = { 
+#      DisableTelemetry = true;
+#      DisableFirefoxStudies = true;
+#      EnableTrackingProtection = {
+#        Value = true;
+#        Locked = true;
+#        Cryptomining = true;
+#        Fingerprinting = true;
+#      };
+#      DisablePocket = true;
+#      DisableFirefoxAccounts = true;
+#      DisableAccounts = true;
+#      DisableFirefoxScreenshots = true;
+#      OverrideFirstRunPage = "";
+#      OverridePostUpdatePage = "";
+#      DontCheckDefaultBrowser = true;
         
-      ExtensionSettings = {
-        "*".installation_mode = "blocked";
-        "uBlock@raymondhill.net" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
-          installation_mode = "force_installed";
-        };
-      };
-    };
-  };
-}
+#      ExtensionSettings = {
+#        "*".installation_mode = "blocked";
+#        "uBlock@raymondhill.net" = {
+#          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+#          installation_mode = "force_installed";
+#        };
+#        "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+#          install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+#          installation_mode = "force_installed";
+#        };
+#      };
+#    };
+#  };
+#}
